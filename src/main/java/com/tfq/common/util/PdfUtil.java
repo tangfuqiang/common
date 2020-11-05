1771670bfeba93ad6874f413380aca12aadb93a9
tangfuqiang <lovetang2017@gmail.com>
2020-11-05 23:17:07 +0800

    1、pdf转word（中文乱码，图片未转待修复）

--- src/main/java/com/tfq/common/util/PdfUtil.java
+++ src/main/java/com/tfq/common/util/PdfUtil.java
@@ -1,7 +1,14 @@
 package com.tfq.common.util;
 
-import java.io.File;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+
+@Slf4j
 public class PdfUtil {
 
     /**
@@ -9,7 +16,44 @@ public class PdfUtil {
      * @param pdfFile pdf文件
      * @param savePath 转化出word保存路径
      */
-    public static void pdfToWord(File pdfFile,String savePath){
+    public static void pdfToWord(File pdfFile,String savePath) throws IOException {
+        Writer writer = null;
+        FileOutputStream outputStream = null;
+        File wordFile = null;
+        try {
+            PDDocument document = PDDocument.load(pdfFile);
+            //获取pdf页数
+            int page = document.getNumberOfPages();
+            String pdfName = pdfFile.getName();
+            savePath = savePath + File.separator + pdfName.substring(0,pdfName.length()-4) + ".doc";
+            wordFile = new File(savePath);
+            if(wordFile.exists()){
+                wordFile.delete();
+            }
+            wordFile.createNewFile();
+            outputStream = new FileOutputStream(wordFile);
+            writer = new OutputStreamWriter(outputStream, "UTF-8");
+            PDFTextStripper stripper = new PDFTextStripper();
+            //排序
+            stripper.setSortByPosition(true);
+            // 设置转换的开始页
+            stripper.setStartPage(1);
+            // 设置转换的结束页
+            stripper.setEndPage(page);
+            stripper.writeText(document,writer);
+        }catch (IOException e){
+            log.error("pdf转图片失败，pdf文件："+pdfFile.getAbsolutePath()+",word文件保存地址："+savePath);
+            if(wordFile.exists()){
+                wordFile.delete();
+            }
+        }finally {
+            if(writer != null){
+                writer.close();
+            }
+            if(outputStream != null){
+                outputStream.close();
+            }
+        }
 
     }
 
@@ -38,5 +82,11 @@ public class PdfUtil {
      */
     public static void wordToPdf(String wordPath,String savePath){
 
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("/Users/tangfuqiang/Downloads/集合.pdf");
+        PdfUtil.pdfToWord(file,"/Users/tangfuqiang/Downloads");
     }
 }
